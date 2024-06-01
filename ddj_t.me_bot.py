e8dc3c25796154b3d040d5294f33e8a424c4d206
John Olakunle <olaksjohnolawale@gmail.com>
2024-06-01 10:14:13 +0100

    Simple telebot script
    Updates are to come soon

--- ddj_t.me_bot.py
+++ ddj_t.me_bot.py
@@ -1,1 +1,14 @@
-packages
+import telebot
+
+# Replace 'YOUR_BOT_TOKEN_HERE' with your actual bot token obtained from BotFather
+bot = telebot.TeleBot('YOUR_BOT_TOKEN_HERE')
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, 'Hello, I\'m your Telegram bot!')
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+bot.polling()
